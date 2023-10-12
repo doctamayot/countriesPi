fd2ef509d73f6b899f54e5530ebbc53b60f744d9
Hugo Tamayo <doctamayot@hotmail.com>
2023-10-12 11:32:33 -0500

    Dia 1

---
+++ server/src/models/Activity.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("Activity", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      unique: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    season: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dificulty: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5,
+        isNumeric: true,
+        isInt: true,
+      },
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      isInt: true,
+      isNumeric: true,
+    },
+  });
+};
---
+++ server/src/models/Country.js
@@ -0,0 +1,43 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("Country", {
+    id: {
+      type: DataTypes.STRING,
+      validate: {
+        is: /^[A-Za-z]{3}$/, // Expresión regular para validar que sean 3 letras
+      },
+      primaryKey: true,
+      allowNull: false,
+      unique: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    img: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subRegion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.FLOAT,
+    },
+    poblacion: {
+      type: DataTypes.INTEGER,
+    },
+  });
+};
